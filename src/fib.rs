6b59c1ed23397b7184c1ee9dacb179378bcbb27f
leghadjeu.christian <legochristianarthur@gmail.com>
2025-03-01 20:59:55 +0100

    Update fib.rs

--- src/fib.rs
+++ src/fib.rs
@@ -2,21 +2,21 @@ use num_bigint::{BigUint, ToBigUint};
 
 /// Computes the nth Fibonacci number using BigInt.
 pub fn fib(n: u32) -> BigUint {
-    let mut a = 0.to_biguint().unwrap();
-    let mut b = 1.to_biguint().unwrap();
+    let mut f0 = 0.to_biguint().unwrap();
+    let mut f1 = 1.to_biguint().unwrap();
 
     if n == 0 {
-        a
+        f0
     } else if n == 1 {
-        b
+        f1
     } else {
         for _ in 2..=n {
-            let next = &a + &b;
-            a = b;
-            b = next;
+            let f2 = &f0 + &f1;
+            f0 = f1;
+            f1 = f2;
         }
 
-        b
+        f1
     }
 }
 
@@ -33,8 +33,8 @@ mod tests {
             BigUint::from_str("354224848179261915075").unwrap()
         );
         assert_eq!(
-            fib(1000),
-            BigUint::from_str("43466557686937456435688527675040625802564660517371780402481729089536555417949051890403879840079255169295922593080322634775209689623239873322471161642996440906533187938298969649928516003704476137795166849228875").unwrap()
+            fib(22),
+            BigUint::from_str("17711").unwrap()
         );
     }
 }
