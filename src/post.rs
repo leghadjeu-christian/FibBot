95812c77121866b5315df6712f9ae393ef104bc7
leghadjeu.christian <legochristianarthur@gmail.com>
2025-03-01 19:03:08 +0100

    Update post.rs

--- src/post.rs
+++ src/post.rs
@@ -1,6 +1,5 @@
-use std::fmt::format;
 
-use reqwest::{header, Client, Error, Response, Url};
+use reqwest::{Client, Error};
 use serde::Serialize;
 #[derive(Serialize)]
 struct Comment {
@@ -8,27 +7,32 @@ struct Comment {
 }
 
 #[tokio::main]
-pub async fn post_comment( owner: &str, repo: &str, pr_num: u32, token: String, comments: String) -> Result<String, Error> { 
-    // https://api.github.com/repos/FibBot/issues/1/comments
+pub async fn post_comment(
+    owner: &str,
+    repo: &str,
+    pr_num: u32,
+    token: String,
+    comments: String,
+) -> Result<String, Error> {
+ 
 
     let client = Client::new();
 
-
-
     let comment = Comment { body: comments };
-    
+
     let response = client
-    .post(format!("https://api.github.com/repos/{owner}/{repo}/issues/{pr_num}/comments" ))
-  .header("Accept", "application/vnd.github+json")
-   .header("Authorization", format!("Bearer {}", token).as_str()) 
-   .header("X-GitHub-Api-Version", "2022-11-28") 
-   .header("User-Agent", format!("{}", owner).as_str())
-    .json(&comment)
-    .send()
-    .await?;
+        .post(format!(
+            "https://api.github.com/repos/{owner}/{repo}/issues/{pr_num}/comments"
+        ))
+        .header("Accept", "application/vnd.github+json")
+        .header("Authorization", format!("Bearer {}", token).as_str())
+        .header("X-GitHub-Api-Version", "2022-11-28")
+        .header("User-Agent", format!("{}", owner).as_str())
+        .json(&comment)
+        .send()
+        .await?;
 
     println!("{:#?}", response);
 
-   Ok(comment.body)
-
+    Ok(comment.body)
 }
